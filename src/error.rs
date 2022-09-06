78d26760d2a54a98483b932437785a0cd2ab2cc9
Danik_Vitek <x3665107@gmail.com>
2022-09-06 21:21:28 +0300

    Improved implementation

--- src/error.rs
+++ src/error.rs
@@ -1,28 +1,27 @@
 use std::io::Error as IoError;
 
 use image::ImageError;
-use pixels::Error as PixelsError;
+use show_image::error::{CreateWindowError, InvalidWindowId, SetImageError};
 use thiserror::Error;
-use winit::error::OsError;
 
 pub type Result<T> = std::result::Result<T, ViewError>;
 
 #[derive(Debug, Error)]
 pub enum ViewError {
+    #[error("Error occured: invalid window id")]
+    OsWindowError(#[from] InvalidWindowId),
+
     #[error("Unable to create a window")]
-    WindowError(#[from] OsError),
+    ShowImageWindowError(#[from] CreateWindowError),
+
+    #[error("Unable to set the image")]
+    SetImageWindowError(#[from] SetImageError),
 
     #[error("Unable to find or open a file")]
     FileError(#[from] IoError),
 
     #[error("Unable to open an image")]
     ImageError(#[from] ImageError),
-
-    #[error("Primary monitor was not found")]
-    NoPrimaryMonitor,
-
-    #[error("Unable to create a pixel buffer to display the image")]
-    PixelBufferError(#[from] PixelsError),
 
     #[error("Unsupported image type")]
     UnsupportedImageType,
